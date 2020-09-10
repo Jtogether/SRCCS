6b39855b696a4314ad2eb72ed627deb6978108cf
newsunny <50794109+newsunny@users.noreply.github.com>
2020-09-10 11:21:30 +0800

    add advancePay

---
+++ phone/advancePay_add/js/advancePay_add.js
@@ -0,0 +1,9 @@
+(function($) {
+    appcan.button("#nav-left", "btn-act",
+    function() {
+        appcan.window.close(-1);
+    });
+    appcan.button("#nav-right", "btn-act",
+    function() {});
+	
+})($);
